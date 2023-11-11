810e3c098a861226a0cf8d1b7f2bee8c21905cda
resonanceee <res0nancee@pm.me>
2023-11-11 01:41:41 +0100

    🥵Idk something changed we are so happy it works

--- ML/test.js
+++ ML/test.js
@@ -1,17 +1,49 @@
 import ffmpeg from 'fluent-ffmpeg';
 
-ffmpeg()
-    .input('test.mp4') // Specify the input file here
-    .on('filenames', (filenames) => {
+function getVideoDuration(videoPath) {
+  return new Promise((resolve, reject) => {
+    ffmpeg.ffprobe(videoPath, (err, metadata) => {
+      if (err) {
+        reject(err);
+      } else {
+        const duration = metadata.format.duration;
+        resolve(duration);
+      }
+    });
+  });
+}
+
+const videoPath = 'test.mp4';
+
+getVideoDuration(videoPath)
+  .then(duration => {
+    console.log(`Video duration: ${duration} seconds`);
+
+    // Generate dynamic timemarks based on the video duration
+    const numberOfScreenshots = duration; // Adjust as needed
+    const timemarks = [];
+
+    for (let i = 1; i <= numberOfScreenshots; i++) {
+      const timemark = (i / (numberOfScreenshots + 1)) * duration;
+      timemarks.push(timemark);
+    }
+
+    ffmpeg()
+      .input(videoPath)
+      .on('filenames', (filenames) => {
         console.log('created', filenames);
-    })
-    .on('end', () => {
+      })
+      .on('end', () => {
         console.log('finito processing');
-    })
-    .on('error', (err) => {
+      })
+      .on('error', (err) => {
         console.error('Error:', err);
-    })
-    .takeScreenshots({
-        filename: 'test.png',
-        timemarks: [1, 2],
-    }, 'images');
+      })
+      .takeScreenshots({
+        filename: 'test%d.png',
+        timemarks: timemarks,
+      }, 'images');
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
