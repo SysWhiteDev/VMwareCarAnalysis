142a2b2034e73d44dd77232565990b68ec3a2ca3
resonanceee <res0nancee@pm.me>
2024-02-12 11:47:27 +0100

    Complete restyle of client container and added rubik font

--- client/Containers/Testing/pages/index.vue
+++ client/Containers/Testing/pages/index.vue
@@ -1,27 +1,5 @@
 <template>
     <div class="wrapper">
         <default/>
-        <battery />
     </div>
 </template>
-
-<style>
-body {
-    margin-top: 0;
-    margin-left: var(--sidebar-width);
-    padding: 0;
-    box-sizing: border-box;
-    font-family: 'Courier New', Courier, monospace;
-}
-.wrapper {
-    display: flex;
-    flex-direction: row;
-    background-color: #235f3c;
-    width: 100vw;
-    height: 100vh;
-}
-</style>
-
-<script>
-
-</script>
